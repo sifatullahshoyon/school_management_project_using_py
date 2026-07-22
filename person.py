5bd460cabaec3274206401ce9835cde48d41663f
Sifat Ullah Shoyon <sifatullah.shoyon@gmail.com>
2026-07-22 19:24:02 +0600

    classroom class

--- person.py
+++ person.py
@@ -20,7 +20,7 @@ class Student(Person):
         self.subject_grade = {} # {"eng" : 'A'}
         self.grade = None # Final Grade
 
-    def final_grade(self):
+    def calculate_final_grade(self):
         sum = 0
         for grade in self.subject_grade.values():
             point = School.grade_to_value(grade) # 5.00
